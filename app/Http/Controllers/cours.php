cf18a70248c8059a4160fd97211cc8b2d6abd621
ilhem-hammami <hammamiilhem20@gmail.com>
2021-08-25 13:04:25 +0200

    firs

---
+++ app/Http/Controllers/cours.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class cours extends Controller
+{ 
+    public function index ()
+    {
+        echo 'hi this is cours controller.';
+    }
+    //
+}
---
+++ app/Http/Controllers/spécialités.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class spécialités extends Controller
+{
+    //
+}
